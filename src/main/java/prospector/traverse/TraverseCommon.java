548bb3997d509fbde025362a2366ddab7322cff9
Prospector <thedoctorsoda@gmail.com>
2017-05-16 19:36:50 -0700

    Recipes & Ore dict

--- src/main/java/prospector/traverse/TraverseCommon.java
+++ src/main/java/prospector/traverse/TraverseCommon.java
@@ -1,12 +1,26 @@
 package prospector.traverse;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 import prospector.traverse.init.TraverseBlocks;
 import prospector.traverse.world.TraverseWorld;
 
 public class TraverseCommon {
+
+    static void registerShaped(ItemStack output, Object... inputs) {
+        GameRegistry.addRecipe(new ShapedOreRecipe(output, inputs));
+    }
+
+    static void registerShapeless(ItemStack output, Object... inputs) {
+        GameRegistry.addRecipe(new ShapelessOreRecipe(output, inputs));
+    }
 
     public void preInit(FMLPreInitializationEvent event) {
         TraverseBlocks.initialize();
@@ -14,6 +28,15 @@ public class TraverseCommon {
 
     public void init(FMLInitializationEvent event) {
         TraverseWorld.init();
+        for (String name : TraverseBlocks.oreDictNames.keySet()) {
+            OreDictionary.registerOre(name, TraverseBlocks.oreDictNames.get(name));
+        }
+        registerShapeless(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks")), 4), new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_log"))));
+        registerShaped(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_slab")), 6), "ppp", 'p', new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks"))));
+        registerShaped(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_stairs")), 4), "p  ", "pp ", "ppp", 'p', new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks"))));
+        registerShaped(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_door")), 3), "pp", "pp", "pp", 'p', new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks"))));
+        registerShaped(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_fence")), 3), "psp", "psp", 's', "stickWood", 'p', new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks"))));
+        registerShaped(new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_fence_gate"))), "sps", "sps", 's', "stickWood", 'p', new ItemStack(Item.getItemFromBlock(TraverseBlocks.blocks.get("fir_planks"))));
     }
 
     public void postInit(FMLPostInitializationEvent event) {
