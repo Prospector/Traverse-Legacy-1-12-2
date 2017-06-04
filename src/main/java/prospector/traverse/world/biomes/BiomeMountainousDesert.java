9311eac1bc1615b9c86497143f1eaceb43f5b401
Prospector <thedoctorsoda@gmail.com>
2017-06-04 10:01:23 -0700

    Add stone decorations to the mountainous desert

--- src/main/java/prospector/traverse/world/biomes/BiomeMountainousDesert.java
+++ src/main/java/prospector/traverse/world/biomes/BiomeMountainousDesert.java
@@ -1,11 +1,21 @@
 package prospector.traverse.world.biomes;
 
 import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeDesert;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
+import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import prospector.traverse.world.ITreeConstants;
+import prospector.traverse.world.features.WorldGenPatch;
+
+import java.util.Random;
 
 public class BiomeMountainousDesert extends BiomeDesert implements ITreeConstants {
 
+    protected static final WorldGenPatch STONE_PATCH_FEATURE = new WorldGenPatch(Blocks.STONE.getDefaultState(), 6, Blocks.SAND, Blocks.SANDSTONE);
+    protected static final WorldGenBlockBlob COBBLESTONE_BOULDER_FEATURE = new WorldGenBlockBlob(Blocks.COBBLESTONE, 1);
     public static BiomeProperties properties = new BiomeProperties("Mountainous Desert");
 
     static {
@@ -18,5 +28,27 @@ public class BiomeMountainousDesert extends BiomeDesert implements ITreeConstant
 
     public BiomeMountainousDesert() {
         super(properties);
+    }
+
+    @Override
+    public void decorate(World worldIn, Random rand, BlockPos pos) {
+        int stoneChance = rand.nextInt(9);
+        if (stoneChance == 0) {
+            int k6 = rand.nextInt(16) + 8;
+            int l = rand.nextInt(16) + 8;
+            BlockPos blockpos = worldIn.getHeight(pos.add(k6, 0, l));
+            STONE_PATCH_FEATURE.generate(worldIn, rand, blockpos);
+        }
+
+        if (net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, pos, DecorateBiomeEvent.Decorate.EventType.ROCK)) {
+            int genChance = rand.nextInt(9);
+            if (genChance == 0) {
+                int k6 = rand.nextInt(16) + 8;
+                int l = rand.nextInt(16) + 8;
+                BlockPos blockpos = worldIn.getHeight(pos.add(k6, 0, l));
+                COBBLESTONE_BOULDER_FEATURE.generate(worldIn, rand, blockpos);
+            }
+        }
+        super.decorate(worldIn, rand, pos);
     }
 }
